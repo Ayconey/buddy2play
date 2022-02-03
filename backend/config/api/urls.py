1273c30ee022c65719b44cdafaeaa881e79b477a
Aycone <ayconey@gmail.com>
2022-02-03 10:02:56 +0100

    initial profile and search

--- backend/config/api/urls.py
+++ backend/config/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path,include
-from .views import UserList,UserDetail,ProfileDetail,ProfileList
+from .views import UserList,UserDetail,ProfileDetail,ProfileList,registerUserProfile,deleteUserByUsername,UserSearch
 
 urlpatterns = [
     path('api-auth/',include('rest_framework.urls')),
     path('users/',UserList.as_view()),
     path('users/<int:pk>/',UserDetail.as_view()),
     path('users/<int:pk>/profile/',ProfileDetail.as_view()),
-    path('users/data/',ProfileList.as_view()),
+    path('users/profile/',ProfileList.as_view()),
+    path('users/update_profile/',registerUserProfile),
+    path('users/dbu/',deleteUserByUsername),
+    path('users/searched/',UserSearch),
 ]
--- backend/config/api/views.py
+++ backend/config/api/views.py
@@ -1,10 +1,104 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
-from rest_framework import generics
-from .serializers import UserSerializer,UserProfileSerializer
+from rest_framework import permissions, generics, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import UserSerializer, UserProfileSerializer
 from UserData.models import Profile
+
 # Create your views here.
 User = get_user_model()
+
+@api_view(['POST'])
+def registerUserProfile(request):  # vulnerable
+    resp = {}
+    # data
+    username = request.data['username']
+    name = request.data['name']
+    surname = request.data['surname']
+    gender = request.data['gender']
+    country = request.data['country']
+    city = request.data['city']
+    sport = request.data['sport']
+    # validation
+    if len(name) == 0 or len(surname) == 0 or len(gender) == 0 or len(country) == 0 or len(city) == 0 or len(
+            sport) == 0 or len(username) == 0:
+        resp['message'] = "one or more fields left empty"
+        return Response(resp, status=status.HTTP_400_BAD_REQUEST)
+
+    # if passed validation
+    selected_user = User.objects.get(username=username)
+    selected_profile = Profile.objects.get(user=selected_user)
+    selected_profile.name = name
+    selected_profile.surname = surname
+    selected_profile.gender = gender
+    selected_profile.country = country
+    selected_profile.city = city
+    selected_profile.sport = sport
+    selected_profile.save()
+    resp['message'] = "Success"
+    return Response(resp, status=status.HTTP_201_CREATED)
+
+
+@api_view(["GET","POST"])
+def UserSearch(request): # try using try and except
+    print(request.data)
+    try:
+        sport = request.data['sport']
+    except:
+        sport = ""
+    try:
+        lft = request.data['lft']
+    except:
+        lft = ""
+    try:
+        lfcg = request.data['lfcg']
+    except:
+        lfcg = ""
+    try:
+        gender = request.data['gender']
+    except:
+        gender = ""
+    try:
+        age_min = request.data['age_min']
+    except:
+        age_min = ""
+    try:
+        age_max = request.data['age_max']
+    except:
+        age_max = ""
+    try:
+        country = request.data['country']
+    except:
+        country = ""
+    try:
+        city = request.data['city']
+    except:
+        city = ""
+
+    selected_users = Profile.objects.all()
+
+    if sport:
+        selected_users = selected_users.filter(sport=sport)
+    if gender:
+        selected_users = selected_users.filter(gender=gender)
+    if country:
+        selected_users = selected_users.filter(country=country)
+    if city:
+        selected_users = selected_users.filter(city=city)
+
+    serializer = UserProfileSerializer(selected_users, many=True)
+    return Response(data=serializer.data)
+
+
+# class UserSearch(generics.ListAPIView):
+#     serializer_class = UserProfileSerializer
+#
+#     def get_queryset(self):
+#         print(self.request)
+#         selected_users = Profile.objects.all()
+#
+#         return selected_users
 
 
 class UserList(generics.ListAPIView):
@@ -22,6 +116,16 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
             return self.request.user
         return self.queryset.get(pk=pk)
 
+
+@api_view(['DELETE',"POST"])
+def deleteUserByUsername(request):
+    resp = {'message':'success'}
+    username = request.data['username']
+    selected_user = User.objects.get(username=username)
+    selected_user.delete()
+    return Response(resp, status=status.HTTP_200_OK)
+
+
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = UserProfileSerializer
     queryset = User.objects.all()
@@ -31,8 +135,6 @@ class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
         return self.queryset.get(pk=pk).profile
 
 
-
 class ProfileList(generics.ListCreateAPIView):
     serializer_class = UserProfileSerializer
     queryset = Profile.objects.all()
-
